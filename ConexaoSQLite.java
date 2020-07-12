146c7432e26f2eaea1fb257122f356d31eddb2f7
amandavc <amandavc27@gmail.com>
2020-07-11 21:36:53 -0400

    edit classe

--- ConexaoSQLite.java
+++ ConexaoSQLite.java
@@ -2,52 +2,58 @@ import java.sql.*;
 
 public class ConexaoSQLite {
 
-    private Connection conexao;
+    /* Conecta a um banco de dados (cria o banco se ele não existir) */
 
-    public boolean conectar() {
+    public static Connection conectar() {
 
         try {
-            // Class.forName("org.sqlite.JDBC");
-            this.conexao = DriverManager.getConnection("jdbc:sqlite:test.bd");
 
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            return false;
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection("jdbc:sqlite:test.db");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Erro na conexao: ", e);
         }
-
-        System.out.println("conectou ao bd!");
-
-        return true;
+        // System.out.println("conectou!!!");
     }
 
-    public boolean desconectar() {
-
+    // sobrecarga nos metodos de desconexao - usados de acordo com o q precisa ser
+    // passado nos prm
+    public static void desconectar(Connection con) {
         try {
-            if (this.conexao.isClosed() == false) {
-                this.conexao.close();
+            if (con != null) {
+                con.close();
             }
 
         } catch (SQLException e) {
-
-            System.err.println(e.getMessage());
-            return false;
+            throw new RuntimeException("Erro na desconexao: ", e);
         }
-        System.out.println("desconectou do bd!");
-        return true;
+        // System.out.println("desconectou!!!");
     }
 
-    // Criar os statements para nossos sqls executados
-    public Statement criarStatement() {
+    public static void desconectar(Connection con, PreparedStatement stmt) {
+        desconectar(con);
 
         try {
-            return this.conexao.createStatement();
+            if (stmt != null)
+                stmt.close();
+
         } catch (SQLException e) {
-            return null;
+            throw new RuntimeException("Erro na desconexao: ", e);
         }
+        // System.out.println("desconectou!!!");
     }
 
-    public Connection getConexao() {
-        return this.conexao;
+    public static void desconectar(Connection con, PreparedStatement stmt, ResultSet rs) {
+        desconectar(con, stmt);
+
+        try {
+            if (rs != null)
+                rs.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na desconexao: ", e);
+        }
     }
 
 }
