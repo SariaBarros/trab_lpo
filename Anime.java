a69148d2aea3628ccc70caa7e452ca2719bfdee7
amandavc <amandavc27@gmail.com>
2020-07-14 02:37:32 -0400

    add construtor

--- Anime.java
+++ Anime.java
@@ -1,16 +1,20 @@
 public class Anime extends Animacao {
-    
-    //private int nTemporadas;
 
-    public Anime(int id, String nome, String classificacaoIndicativa, String estudio, String genero/*,  Temporada[] temporada */) {
+    // private int nTemporadas = 0;
+
+    public Anime(int id, String nome, String classificacaoIndicativa, String estudio, String genero) {
         super(id, nome, classificacaoIndicativa, estudio, genero);
-        //setTemporada(temporada);
     }
-    
+
+    // construtor sobrecarregado usado para objetos
+    // auxiliares no cadastro de animaçoes
+    public Anime(int fk) {
+        super(fk, null, null, null, null);
+    }
+
     @Override
     public String toString() {
         return "id: " + getId() + "      Anime: " + getNome() + "\nClassificaçao indicativa: "
                 + getClassificacaoIndicativa() + "\nEstudio: " + getEstudio() + "\nGenero: " + getGenero() + "\n";
     }
-
 }
