a74ac60fe48580779948f855cd924ce3d6175f94
amandavc <amandavc27@gmail.com>
2020-07-17 17:43:42 -0400

    novo construtor

--- Anime.java
+++ Anime.java
@@ -1,20 +1,23 @@
 public class Anime extends Animacao {
 
-    // private int nTemporadas = 0;
-
+    //construtor para o CREATE e UPDATE - sem id
+    public Anime(String nome, String classificacaoIndicativa, String estudio, String genero) {
+        super(nome, classificacaoIndicativa, estudio, genero);
+    }
+    
+    //contrutor padrão
     public Anime(int id, String nome, String classificacaoIndicativa, String estudio, String genero) {
         super(id, nome, classificacaoIndicativa, estudio, genero);
     }
 
-    // construtor sobrecarregado usado para objetos
-    // auxiliares no cadastro de animaçoes
+    // construtor usado para objetos auxiliares no cadastro de animaçoes
     public Anime(int fk) {
         super(fk, null, null, null, null);
     }
 
     @Override
     public String toString() {
-        return "id: " + getId() + "      Anime: " + getNome() + "      \nClassificaçao indicativa: "
+        return "idA: " + getId() + "        Anime: " + getNome() + "      \nClassificaçao indicativa: "
                 + getClassificacaoIndicativa() + "      \nEstudio: " + getEstudio() + "      \nGenero: " + getGenero() + "\n";
     }
 }
