a9abfc96a6235ae5afc951d7db29a42a3c14cae6
amandavc <amandavc27@gmail.com>
2020-07-20 16:39:05 -0400

    final main

--- Main.java
+++ Main.java
@@ -4,52 +4,6 @@ public class Main {
 
         JanelaComposicao janela = new JanelaComposicao("Cadastrar Animacoes");
         
-        /*
-        Leitor leitor = new Leitor();
-        int menu;
-        Animacao[] animacoes = new Animacao[10];
-        int quantAnimacao = 0;
-        FilmeDAO filmedao = new FilmeDAO();
         
-        do {
-            System.out.println("Escolha uma opção:");
-            System.out.println("[1] Cadastrar um Filme");
-            System.out.println("[2] Cadastrar um Anime");
-            System.out.println("[3] Sair");
-
-            menu = leitor.leInteiro();
-        
-            switch (menu) {
-                case 1:
-                    System.out.println("Digite o nome: ");
-                    String nome = leitor.leString();
-                    System.out.println("Digite qual eh a classificacao indicada: ");
-                    String classificacaoIndicativa = leitor.leString();
-                    System.out.println("Digite qual eh o estudio: ");
-                    String estudio = leitor.leString();
-                    System.out.println("Digite o genero: ");
-                    String genero = leitor.leString();
-                    System.out.println("Digite o ano que o filme foi criado: ");
-                    int ano = leitor.leInteiro();
-                    System.out.println("Digite a duracao do filme: ");
-                    String duracao = leitor.leString();
-
-                    animacoes[quantAnimacao] = new Filme(nome, classificacaoIndicativa, estudio, genero, ano, duracao);
-                    Filme filme = (Filme) animacoes[quantAnimacao];
-                    filmedao.create(filme);
-                    
-                    quantAnimacao++;
-                    break;
-            
-                case 2:
-
-
-                    break;
-
-                default:
-                    break;
-            }
-        } while (menu != 3); */
-       
     }
 }
